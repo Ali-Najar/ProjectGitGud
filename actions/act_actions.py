36049ef26cc073f11a47928d87c7e4187de84ef1
Ali Najar <119691638+Ali-Najar@users.noreply.github.com>
2024-09-06 16:31:58 +0330

    uploaded actions

---
+++ actions/act_actions.py
@@ -0,0 +1,13 @@
+
+Actions = {
+            0:['space'],   #dodge
+            1:['l'],   #light
+            2:['h'],   #heavy
+            3:['r'],   #item
+            4:[]      #idle
+}
+
+Dodge = {
+            0:['space'],   #dodge
+            1:[]      #idle
+}
---
+++ actions/cam_actions.py
@@ -0,0 +1,22 @@
+
+Actions = {
+            0:['j'],   #cam up
+            1:['k'],   #cam down
+            2:['o'],   #cam left
+            3:['p'],   #cam right
+            4:['q'],   #lock
+            5:[]      #idle
+}
+
+Camera = {
+    0:['j'],   #cam up
+    1:['k'],   #cam down
+    2:['o'],   #cam left
+    3:['p'],   #cam right
+    4:[]      #idle
+}
+
+Lock_On = {
+    0:['q'],   #lock
+    1:[]      #idle
+}
